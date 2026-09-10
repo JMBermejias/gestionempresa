d0e68433b877ca3665198e3691d67162bbd8c2fe
jmbernabeu <jmbernabeu@users.noreply.github.com>
2026-09-10 08:41:06 +0200

    feat: add presupuestos and facturacion modules (v1.1.0)

--- backend/medicion_server.py
+++ backend/medicion_server.py
@@ -12,7 +12,7 @@ from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 DB_PATH = os.environ.get('MEDICION_DB', '/var/lib/gestion-empresa/medicion.db')
 HOST = os.environ.get('MEDICION_HOST', '127.0.0.1')
 PORT = int(os.environ.get('MEDICION_PORT', '8000'))
-COLLECTIONS = ['materials', 'mediciones', 'empresas', 'obras', 'zonas', 'subcontratas']
+COLLECTIONS = ['materials', 'mediciones', 'empresas', 'obras', 'zonas', 'subcontratas', 'presupuestos', 'facturas']
 
 SCHEMA = (
     'CREATE TABLE IF NOT EXISTS appdata ('
